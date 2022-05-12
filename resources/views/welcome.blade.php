41d37ed4427152c6fadee9c5c679a5c1b69c1925
MaxAlgavaBernabe <71739044+MaxAlgavaBernabe@users.noreply.github.com>
2022-05-11 19:07:18 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<HEAD>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/index.png">
+</HEAD>
+<title>Postres Tiwis</title>
+<body>
+<nav>
+    <ul>
+    <li><a href="#a">Mermelada</a></li>
+    <li><a href="#b">Pan Mora</a></li>
+    <li><a href="#c">Pastel</a></li>
+    <li> <a href="#d">Pan Cafe</a></li>
+    <li><a href="#e">Galletas</a></li>
+    <li><a href="#f">Pizza</a></li>
+    <li><a href="#g">Tortilla de Harina</a></li>
+    <li><a href="#h">Donas en Horno</a></li>
+
+    </ul>
+</nav>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div id="a">
+    
+    <h1>Mermelada fresa con manzana</h2>
+    <img src="img/1.jpeg" width="300" height="200">
+</div>
+<div id="b">
+    <h1>Pan Mora</h2>
+    <img src="img/2.jpeg" width="300" height="200">
+</div>
+<div id="c">
+    <h1>Pastel</h2>
+    <img src="img/3.jpeg" width="300" height="200">
+</div>
+<div id="d">
+    <h1>Pan cafe</h2>
+    <img src="img/4.jpeg" width="300" heigth="200">
+</div>
+<div id="e">
+    <h1>Gallatas</h2>
+    <img src="img/5.jpeg" width="300" heigth="200">
+</div>
+<div id="f">
+    <h1>Pizza</h2>
+    <img src="img/6.jpeg" width="300" heigth="200">
+</div>
+<div id="g">
+    <h1>Tortilla de Harina</h2>
+    <img src="img/7.jpg" width="300" heigth="200">
+</div>
+
+<div id="h">
+    <h1>Dona en Horno</h2>
+    <img src="img/8.jpeg" width="300" heigth="200">
+</div>
+
+</body>
+
+<footer>
+
+<a href="https://www.facebook.com/Max.Algava.Fsociety">@MaxAlgava</a>
+
+</footer>
+</html>
